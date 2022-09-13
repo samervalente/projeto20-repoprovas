2c82ee7e3a596b2dbd22751ffb50959096432309
samervalente <samervalente@gmail.com>
2022-09-13 09:28:25 -0300

    feat: create tests model

---
+++ prisma/migrations/20220913122813_create_test_model/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "tests" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "pdfUrl" TEXT NOT NULL,
+    "categoryId" INTEGER NOT NULL,
+
+    CONSTRAINT "tests_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "tests" ADD CONSTRAINT "tests_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "categories"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
