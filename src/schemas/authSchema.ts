49d1fd7545c3ad4810746831d4369bc21861688a
samervalente <samervalente@gmail.com>
2022-09-14 07:07:59 -0300

    feat: prepare for deploy

--- src/schemas/authSchema.ts
+++ src/schemas/authSchema.ts
@@ -2,7 +2,8 @@ import joi from "joi"
 
 const userSchema = joi.object({
     email: joi.string().email().required(),
-    password: joi.string().required()
+    password: joi.string().required(),
+    confirmPassword:joi.ref("password")
 })
 
 export default userSchema
